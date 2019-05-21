7c6179b9cdbe1ad0ba55aa8587ba4c728295ed27
sayed.musavy@integrify.io <sayed.musavy@integrify.io>
2019-05-21 12:02:09 +0300

    react assingment post page

---
+++ src/component/posts.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "../component/style/style.css"
+const Posts = () => {
+  return (
+    <div>
+
+        <h1>Welcome to my Pages</h1>
+      <button id="add-btn">Add Post</button>
+      <span>My day in integrify</span> <span>Work</span>
+    </div>
+  );
+};
+
+export default Posts;
+
